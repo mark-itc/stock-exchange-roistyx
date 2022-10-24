a67f21a6b1d85148f7da8c463601a2ba639ebc39
Roie A <109058904+roistyx@users.noreply.github.com>
2022-10-24 16:32:06 +0300

    Update Marquee.js

--- script/Marquee.js
+++ script/Marquee.js
@@ -29,17 +29,17 @@ class Marquee {
 
             for (let i = 0; i < 30; i++) {
             let results = [
-                    getResults[	0	].symbol	+	"  "	+	getResults[	0	].price	+	"%   |   "	+
-                    getResults[	1	].symbol	+	"  "	+	getResults[	1	].price	+	"%   |   "	+
-                    getResults[	2	].symbol	+	"  "	+	getResults[	2	].price	+	"%   |   "	+
-                    getResults[	3	].symbol	+	"  "	+	getResults[	3	].price	+	"%   |   "	+
-                    getResults[	4	].symbol	+	"  "	+	getResults[	4	].price	+	"%   |   "	+
-                    getResults[	5	].symbol	+	"  "	+	getResults[	5	].price	+	"%   |   "	+
-                    getResults[	6	].symbol	+	"  "	+	getResults[	6	].price	+	"%   |   "	+
-                    getResults[	7	].symbol	+	"  "	+	getResults[	7	].price	+	"%   |   "	+
-                    getResults[	8	].symbol	+	"  "	+	getResults[	8	].price	+	"%   |   "	+
-                    getResults[	9	].symbol	+	"  "	+	getResults[	9	].price	+	"%   |   "	+
-                    getResults[	10	].symbol	+	"  "	+	getResults[	10	].price	];
+                    getResults[	0	].symbol	+	"  $"	+	getResults[	0	].price	+	"   |   "	+
+                    getResults[	1	].symbol	+	"  $"	+	getResults[	1	].price	+	"   |   "	+
+                    getResults[	2	].symbol	+	"  $"	+	getResults[	2	].price	+	"   |   "	+
+                    getResults[	3	].symbol	+	"  $"	+	getResults[	3	].price	+	"   |   "	+
+                    getResults[	4	].symbol	+	"  $"	+	getResults[	4	].price	+	"   |   "	+
+                    getResults[	5	].symbol	+	"  $"	+	getResults[	5	].price	+	"%   |   "	+
+                    getResults[	6	].symbol	+	"  $"	+	getResults[	6	].price	+	"   |   "	+
+                    getResults[	7	].symbol	+	"  $"	+	getResults[	7	].price	+	"   |   "	+
+                    getResults[	8	].symbol	+	"  $"	+	getResults[	8	].price	+	"   |   "	+
+                    getResults[	9	].symbol	+	"  $"	+	getResults[	9	].price	+	"   |   "	+
+                    getResults[	10	].symbol	+	"  $"	+	getResults[	10	].price	];
                 
                 marquee.innerHTML = `<span >${results}</span>`;
 
